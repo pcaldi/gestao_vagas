2aa58dc8540f76b7a0949a252a7f24e3c51347c3
pcaldi <pcaldi@gmail.com>
2023-11-09 15:26:40 -0300

    Formatando messagem de validacão

--- src/main/java/com/pcaldi/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/pcaldi/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.hibernate.validator.constraints.Length;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
@@ -15,13 +16,14 @@ public class CandidateEntity {
     private UUID id;
     private String name;
 
-    @Pattern(regexp = "(\\S+)", message = "O campo [username] não deve conter espaços")
+    @NotBlank()
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaços")
     private String username;
 
     @Email(message = "O campo [e-mail] deve conter um e-mail válido")
     private String email;
 
-    @Length(min = 10, max = 100)
+    @Length(min = 10, max = 100, message = "O campo [senha] de conter entre 10 e 100 caracteres")
     private String password;
 
     private String description;
