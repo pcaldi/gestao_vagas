352487b3d6b5720f0e643b23038157348a678955
pcaldi <pcaldi@gmail.com>
2023-11-13 19:45:23 -0300

    criptografando senha do canditado

--- src/main/java/com/pcaldi/gestao_vagas/modules/candidate/useCase/CreateCandidateUseCase.java
+++ src/main/java/com/pcaldi/gestao_vagas/modules/candidate/useCase/CreateCandidateUseCase.java
@@ -1,6 +1,7 @@
 package com.pcaldi.gestao_vagas.modules.candidate.useCase;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.pcaldi.gestao_vagas.exceptions.UserFoundExcption;
@@ -13,11 +14,16 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundExcption();
                 });
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
 
         return this.candidateRepository.save(candidateEntity);
 
