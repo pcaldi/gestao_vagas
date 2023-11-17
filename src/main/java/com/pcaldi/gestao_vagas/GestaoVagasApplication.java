19c2417a50470dda43f11d0da7612f5db5e63e10
pcaldi <pcaldi@gmail.com>
2023-11-17 20:43:45 -0300

    Instalando/configurando swagger

--- src/main/java/com/pcaldi/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/com/pcaldi/gestao_vagas/GestaoVagasApplication.java
@@ -3,7 +3,11 @@ package com.pcaldi.gestao_vagas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Gestão de Vagas", description = "API responsável por gestão de vagas", version = "1"))
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
