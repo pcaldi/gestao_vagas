5f49e3bfc8d814573e5770748c9a96c18ca2a4e9
pcaldi <pcaldi@gmail.com>
2023-11-19 10:38:55 -0300

    inserindo exemplos  na requisição

--- src/main/java/com/pcaldi/gestao_vagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/com/pcaldi/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -1,11 +1,18 @@
 package com.pcaldi.gestao_vagas.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import lombok.Data;
 
 @Data
 public class CreateJobDTO {
 
+    @Schema(example = "Vaga de estágio para React Native", requiredMode = RequiredMode.REQUIRED)
     private String description;
+
+    @Schema(example = "GYMPass, VA/VT, Day off no aniversário", requiredMode = RequiredMode.REQUIRED)
     private String benefits;
+
+    @Schema(example = "ESTÁGIO", requiredMode = RequiredMode.REQUIRED)
     private String level;
 }
--- src/main/java/com/pcaldi/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/com/pcaldi/gestao_vagas/modules/company/entities/JobEntity.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -29,10 +30,13 @@ public class JobEntity {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Schema(example = "Vaga de estágio para React Native")
     private String description;
 
+    @Schema(example = "GYMPass, VA/VT, Day off no aniversário")
     private String benefits;
 
+    @Schema(example = "ESTÁGIO")
     @NotBlank(message = "Campo obrigatório")
     private String level;
 
