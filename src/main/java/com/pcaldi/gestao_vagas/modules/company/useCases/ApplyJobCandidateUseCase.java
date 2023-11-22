7ce0724d52ca5f42fd65eae51a551f848098275c
pcaldi <pcaldi@gmail.com>
2023-11-22 20:32:13 -0300

    criando o entity do appy job

--- src/main/java/com/pcaldi/gestao_vagas/modules/company/useCases/ApplyJobCandidateUseCase.java
+++ src/main/java/com/pcaldi/gestao_vagas/modules/company/useCases/ApplyJobCandidateUseCase.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.pcaldi.gestao_vagas.exceptions.JobNotFoundException;
 import com.pcaldi.gestao_vagas.exceptions.UserNotFoundException;
 import com.pcaldi.gestao_vagas.modules.candidate.CandidateRepository;
+import com.pcaldi.gestao_vagas.modules.candidate.repository.ApplyJobRepository;
 import com.pcaldi.gestao_vagas.modules.company.repositories.JobRepository;
 
 public class ApplyJobCandidateUseCase {
@@ -16,6 +17,9 @@ public class ApplyJobCandidateUseCase {
 
     @Autowired
     private JobRepository jobRepository;
+
+    @Autowired
+    private ApplyJobRepository applyJobRepository;
 
     // ID do candidato
     // ID da vaga
